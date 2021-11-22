250c7322f59d48b6a53e61d961612c1579f07e9e
caique <caique.ccali@gmail.com>
2021-11-22 06:04:40 -0300

    changing into favorite musician

--- fav-musician/assets/scripts/script.js
+++ fav-musician/assets/scripts/script.js
@@ -29,48 +29,47 @@ let selMain = document.querySelector("main");
         document.querySelector("#city-and-state").innerHTML = "são paulo, br";
 
         let makeMain = document.createElement("main");
-        // makeMain.setAttribute("id", "hobby-content");
         document.querySelector(".column").appendChild(makeMain);
 
 
-        let makeHobbyTitle = document.createElement("h2");
-        makeHobbyTitle.setAttribute("id", "hobby-title");
-        document.querySelector("main").appendChild(makeHobbyTitle);
-        document.querySelector("#hobby-title").innerHTML = "cats";
+        let makeMusicTitle = document.createElement("h2");
+        makeMusicTitle.setAttribute("id", "music-title");
+        document.querySelector("main").appendChild(makeMusicTitle);
+        document.querySelector("#music-title").innerHTML = "arnaldo antunes";
 
-        let makeHobbySub = document.createElement("p");
-        makeHobbySub.setAttribute("id", "hobby-sub");
-        document.querySelector("main").appendChild(makeHobbySub);
-        document.querySelector("#hobby-sub").innerHTML = "(not the musical)";
+        let makeMusicSub = document.createElement("p");
+        makeMusicSub.setAttribute("id", "music-sub");
+        document.querySelector("main").appendChild(makeMusicSub);
+        document.querySelector("#music-sub").innerHTML = "a crazy, yet free, mind!";
 
         let makeReasonsTitle = document.createElement("h3");
         makeReasonsTitle.setAttribute("id", "reasons-title");
         document.querySelector("main").appendChild(makeReasonsTitle);
-        document.querySelector("#reasons-title").innerHTML = "learn and love";
+        document.querySelector("#reasons-title").innerHTML = "top 3 favorite songs";
 
         let makeReasonsSub = document.createElement("p");
         makeReasonsSub.setAttribute("id", "reasons-sub");
         document.querySelector("main").appendChild(makeReasonsSub);
-        document.querySelector("#reasons-sub").innerHTML = "all you have to do to love cats is learn more about them. for me, the purrrfect way to do so is  by having a cat of your own. but if you\'re not ready, you can learn more about them theoretically first:";
+        document.querySelector("#reasons-sub").innerHTML = "it\'s a very hard thing for me deciding wich of his songs I like the best, because he writes lyrics that are mind blowing. sarcastic portraits of society, sassy observations on human behavior, acid comments on the status quo. this and always a playful way of habdling our amazing mother language: the brazilian portuguese.";
 
         let makeUL = document.createElement("ul");
-        makeUL.setAttribute("id", "hobby-refs");
+        makeUL.setAttribute("id", "music-refs");
         document.querySelector("main").appendChild(makeUL);
 
         let makeListItem1 = document.createElement("li");
         makeListItem1.setAttribute("id", "item-1");
-        document.querySelector("#hobby-refs").appendChild(makeListItem1);
-        document.querySelector("#item-1").innerHTML = "chico\'s podcat: he is ceo at <a target='_blank' href='https://www.canseidesergato.com/'>cansei de ser gato (pt-br)</a> and speaks on behalf of the feline cause every monday on spotify;";
+        document.querySelector("#music-refs").appendChild(makeListItem1);
+        document.querySelector("#item-1").innerHTML = "top 1 HAD to be this mockery of gender <a target='_blank' href='https://www.youtube.com/watch?v=UB3-7wl0arM'>everyone should listen to it</a>;";
 
         let makeListItem2 = document.createElement("li");
         makeListItem2.setAttribute("id", "item-2");
-        document.querySelector("#hobby-refs").appendChild(makeListItem2);
-        document.querySelector("#item-2").innerHTML = "carlos ruas\' cartoon series called <a target='_blank' href='https://www.umsabadoqualquer.com/category/caes-e-gatos/''>cães e gatos (pt-br)</a>, which is an accurate portrait of cats in comparison to dogs;";
+        document.querySelector("#music-refs").appendChild(makeListItem2);
+        document.querySelector("#item-2").innerHTML = "this one is a very upbeat one about fashion and how amazingly diverse it can be.. <a target='_blank' href='https://www.youtube.com/watch?v=NdLm6A_Y3Mk'>drag your furniture away</a> and just dance around;";
 
         let makeListItem3 = document.createElement("li");
         makeListItem3.setAttribute("id", "item-3");
-        document.querySelector("#hobby-refs").appendChild(makeListItem3);
-        document.querySelector("#item-3").innerHTML = "science stuff about cats, such as <a target='_blank' href='https://edition.cnn.com/2021/11/10/world/cats-track-owners-location-voice-scn/index.html'>this amazing cnn article (en)</a> on how cats use their hearing to keep track of their owners.";
+        document.querySelector("#music-refs").appendChild(makeListItem3);
+        document.querySelector("#item-3").innerHTML = "aaaand one of my favorites to listen to on sad days.. <a target='_blank' href='https://www.youtube.com/watch?v=YRy4ET_4788'> press play now </a> and notice how he keeps playing with words and THAT'S what makes his songs perfect for me!";
 
         let makeFooter = document.createElement("footer");
         makeFooter.setAttribute("id", "cat-gallery");
@@ -79,7 +78,7 @@ let selMain = document.querySelector("main");
         let gridContent = '';
 
         for (let i = 1; i <= 15; i++) {
-            let gridItem = '<img src="assets/images/cat' + i + '.jpg">';
+            let gridItem = '<img src="assets/images/album' + i + '.jpeg">';
             gridContent = gridContent + gridItem;
         }
 
