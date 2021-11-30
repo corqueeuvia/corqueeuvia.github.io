1226686ed02ddba8627cbf9943411bb6b50837df
caique <caique.ccali@gmail.com>
2021-11-30 16:25:04 -0300

    making things work properly

--- bart/assets/script.js
+++ bart/assets/script.js
@@ -3,9 +3,11 @@ const ctx = canvas.getContext('2d');
 let corDoGiz = '#FFFFFF';
 let x = 25;
 let y = 40;
-let phrase = 'Transplantes de órgãos são melhores quando deixados para profissionais';
+let phrase = '';
 let phraseCount = 0;
-let inputNum = document.getElementById('input-num');
+let eraseCount = 0;
+const inputNum = document.getElementById('input-num');
+const phraseCollection = ['EU NÃO GRITAREI “FOGO” EM UMA SALA DE AULA CHEIA', 'EU NÃO SOU UMA VELHA DE 32 ANOS','ASSOPRAR BOLINHAS DE PAPEL NÃO É LIBERDADE DE EXPRESSÃO','CAFÉ NÃO É PARA CRIANÇAS','EU NÃO ESTOU AUTORIZADO A DEMITIR PROFESSORAS SUBSTITUTAS','EU NÃO VOU PASSAR SABÃO NAS ESCADAS','CUECÃO NÃO É SAUDÁVEL ​​PARA AS CRIANÇAS E OUTROS SERES VIVOS','NÃO É NECESSÁRIO HAVER FOGO PARA O TREINAMENTO DE INCÊNDIO','NÃO HÁ NENHUM DEUS ROMANO CHAMADO PEIDACUS','UM MACACO TREINADO NÃO PODE DAR AULA','EU NÃO POSSO TER UM ESTUDANTE SUBSTITUTO','SÓ FORNECEREI AMOSTRA DE URINA QUANDO PEDIDO','UM ARROTO ENGARRAFADO NÃO É PROJETO DE CIÊNCIAS','EU NÃO VOU ESPECULAR SOBRE QUANTO O PROFESSOR PODE SER SEXY','MELECA NÃO É MARCADOR DE LIVRO','JE NE PARLE PAS FRANÇAIS','NÃO ESTAMOS NUS DEBAIXO DAS ROUPAS','A CAPITAL DE MONTANA NÃO É HANNAH','O BATMAN NÃO É NADA SEM SEU CINTO DE UTILIDADES','QUANDO EU DORMI NA SALA DE AULA NÃO ERA PARA AJUDAR O LEO DICAPRIO','AS ÚLTIMAS PALAVRAS DE JESUS NÃO FORAM "GRAÇAS À DEUS É SEXTA-FEIRA"','NÃO SE PODE FAZER PEGADINHA DE 1º DE ABRIL NO DIA 27','BONECOS DE NEVE NÃO TÊM PÊNIS DE CENOURA']
 
 desenhaLousa();
 
@@ -32,6 +34,8 @@ function desenhaApagador(x, y) {
 function limpaLousa() {
     ctx.fillStyle = '#142918';
     ctx.fillRect(20, 20, 760, 340);
+    ctx.fillStyle = '#fff';
+    eraseCount++;
 }
 
 function desenhaLousa() {
@@ -48,35 +52,32 @@ function desenhaLousa() {
     desenhaGiz('#FF4C7A', 300, 377); //rosa
     desenhaApagador(400, 370);
 }
-
-function usaApagador() {
-    var areaCanvas = document.getElementById('canvas');
-    areaCanvas.onmousedown = function (e) {
-        var x = e.pageX - this.offsetLeft;
-        var y = e.pageY - this.offsetTop;
-
-        if (x > 400 &&
-            x < 480 &&
-            y > 350) {
-            limpaLousa();
-        }
-    }
-}
 //função para desenhar texto
 function writePhrase(x, y, phrase) {
-    var ctx = document.getElementById('canvas').getContext('2d');
-    ctx.font = '20px cursive';
+    ctx.fillStyle = '#fff';
+    ctx.font = '16px cursive';
     ctx.fillText(phrase, x, y);
 }
 function punishBart() {
+    resetAll();
+    getRandomSentence();
     let repeat = inputNum.value;
     while (phraseCount < repeat) {
         writePhrase(x, y, phrase);
         phraseCount++;
+        counter++;
         y += 30;
     }
 }
-//código para medir o texto
-//var ctx = document.getElementById('canvas').getContext('2d');
-//var text = ctx.measureText('textoaqui');
-//console.log(text.width);
+function resetAll() {
+    y = 40;
+    limpaLousa();
+    phraseCount = 0;
+    counter = 0;
+}
+function getRandomSentence() {
+    let min = Math.ceil(0);
+    let max = Math.floor(23);
+    let phraseNum = Math.floor(Math.random() * (max - min)) + min;
+    phrase = phraseCollection[phraseNum];
+  }
