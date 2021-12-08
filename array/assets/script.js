1d0d9adf8460013a1154efbbe0e4dcfa5de0eeaf
caique <caique.ccali@gmail.com>
2021-12-08 11:39:03 -0300

    added comments and removed unecessary functions

--- array/assets/script.js
+++ array/assets/script.js
@@ -1,26 +1,32 @@
-// sortAscending()();
+//getting all input fields to recieve user's numbers
 const input_a = document.getElementById("in-a");
 const input_b = document.getElementById("in-b");
 const input_c = document.getElementById("in-c");
 const input_d = document.getElementById("in-d");
 
+//getting all output fields to display results
 const output_a = document.getElementById("out-a");
 const output_b = document.getElementById("out-b");
 const output_c = document.getElementById("out-c");
 const output_d = document.getElementById("out-d");
 
+//variables to store numbers
 let a = 0;
 let b = 0;
 let c = 0;
 let d = 0;
 
+//storage verification
 let saved = false;
 
+//get values from input fields and stores into variables
 function saveNumbers() {
     a = input_a.value;
     b = input_b.value;
     c = input_c.value;
     d = input_d.value;
+
+    //error verification not to store empty values
     if (a === '' &&
         b === '' &&
         c === '' &&
@@ -37,6 +43,7 @@ function saveNumbers() {
     }
 }
 
+//get values into an array and reverse its position
 function reverseNumbers() {
     if (saved) {
         let inputNumbers = [a, b, c, d];
@@ -58,6 +65,7 @@ function reverseNumbers() {
     }
 }
 
+//sort values and display them in ascending order
 function sortAscending() {
     if (saved) {
         let inputNumbers = [parseInt(a), parseInt(b), parseInt(c), parseInt(d)];
@@ -76,6 +84,7 @@ function sortAscending() {
     }
 }
 
+//generate random numbers for the input fields so user doesn't need to pick any
 function getRandomNumbers() {
     input_a.value = getRandomInt(0, 1000);
     input_b.value = getRandomInt(20, 10000);
@@ -83,12 +92,14 @@ function getRandomNumbers() {
     input_d.value = getRandomInt(50, 500);
 }
 
+//get a random number in specified range (used in getRandomNumbers above)
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+//send values into output fields after sorting, reversing or saving
 function displayNumbers() {
     output_a.value = a;
     output_b.value = b;
@@ -96,14 +107,7 @@ function displayNumbers() {
     output_d.value = d;
 }
 
-function clearMemory() {
-    memo_a = '';
-    memo_b = '';
-    memo_c = '';
-    memo_d = '';
-    saved = false;
-}
-
+//clear outputfields not to display any value (used in resetPage function)
 function clearOutput() {
     output_a.value = '';
     output_b.value = '';
@@ -111,6 +115,7 @@ function clearOutput() {
     output_d.value = '';
 }
 
+//clear input fields so they don't show any value (used in resetPage and saveNumbers functions)
 function clearInput() {
     input_a.value = '';
     input_b.value = '';
@@ -118,12 +123,13 @@ function clearInput() {
     input_d.value = '';
 }
 
+//reset page to its initial state (used on button reset)
 function resetPage() {
-    clearMemory();
     clearOutput();
     clearInput();
 }
 
+//display custom error message into output fields depending on the action attempted
 function showError(action) {
     output_a.value = '#### ERROR! ####';
     output_b.value = '<------------------------';
