df9bd941fb8781de074b0e0e85bfcd4dc30de1f7
caique <caique.ccali@gmail.com>
2022-01-27 16:46:13 -0300

    added rest activity

---
+++ rest/assets/script.js
@@ -0,0 +1,47 @@
+//question: make a function that can receive infinite params and arrange them all into an array
+
+function makeArray(...args) {
+    return array = args;
+}
+
+const input = document.getElementById('input');
+const addBtn = document.getElementById('add-btn');
+const doneBtn = document.getElementById('done-btn');
+const newBtn = document.getElementById('new-btn');
+const displayNums = document.getElementById('display-nums');
+const displayArr = document.getElementById('display-arr');
+const savedNums = [];
+
+function addNewNum() {
+    let num = input.value;
+    if (num === '') {
+        displayArr.innerHTML = 'ERR!'
+    } else {
+        savedNums.push(num);
+        displayNums.innerHTML += ` ${num}...`;
+        displayArr.innerHTML = '';
+        input.value = '';
+    }
+
+    return savedNums;
+}
+
+addBtn.onclick = function () { addNewNum() };
+
+function displayArray(...arr) {
+    displayNums.innerHTML = '';
+    input.value = '';
+    return displayArr.innerHTML = `Final array is: ${arr}.`;
+}
+
+doneBtn.onclick = function () { displayArray(savedNums) };
+
+function startNewArr() {
+    displayNums.innerHTML = 'Adding to array: ';
+    displayArr.innerHTML = '';
+    savedNums.splice(0, savedNums.length);
+    input.value = '';
+    return true;
+}
+
+newBtn.onclick = function () { startNewArr() };
