36b066a7a2a64d9f11352021c7a5deaee678f882
caique <caique.ccali@gmail.com>
2022-01-28 09:48:09 -0300

    added comments and removed unnecessary ones

--- rest/assets/script.js
+++ rest/assets/script.js
@@ -1,19 +1,15 @@
 //question: make a function that can receive infinite params and arrange them all into an array
 
-function makeArray(...args) {
-    return array = args;
-}
-
-const input = document.getElementById('input');
-const addBtn = document.getElementById('add-btn');
-const doneBtn = document.getElementById('done-btn');
-const newBtn = document.getElementById('new-btn');
-const displayNums = document.getElementById('display-nums');
+const input = document.getElementById('input');                 //capturing input,
+const addBtn = document.getElementById('add-btn');              //buttons and 
+const doneBtn = document.getElementById('done-btn');            //diplay fields
+const newBtn = document.getElementById('new-btn');              //to show data
+const displayNums = document.getElementById('display-nums');    //and results
 const displayArr = document.getElementById('display-arr');
-const savedNums = [];
+const savedNums = [];                                          //array to store inputed nums
 
-function addNewNum() {
-    let num = input.value;
+function addNewNum() {                  //add new input into
+    let num = input.value;              //array if num is valid
     if (num === '') {
         displayArr.innerHTML = 'ERR! Can\'t add empty value to array.'
     } else {
@@ -26,22 +22,21 @@ function addNewNum() {
     return savedNums;
 }
 
-addBtn.onclick = function () { addNewNum() };
+addBtn.onclick = function () { addNewNum() }; //set button to add new number
 
-function displayArray(...arr) {
-    //displayNums.innerHTML = '';
+function displayArray(...arr) {         //display final array on screen
     input.value = '';
     return displayArr.innerHTML = `Final array is: ${arr}.`;
 }
 
-doneBtn.onclick = function () { displayArray(savedNums) };
+doneBtn.onclick = function () { displayArray(savedNums) }; //set button to display array
 
-function startNewArr() {
-    displayNums.innerHTML = 'Adding to array: ';
-    displayArr.innerHTML = '';
+function startNewArr() {                            //reset savedNums array
+    displayNums.innerHTML = 'Adding to array: ';    //so user can create a
+    displayArr.innerHTML = '';                      //new array from scratch
     savedNums.splice(0, savedNums.length);
     input.value = '';
     return true;
 }
 
-newBtn.onclick = function () { startNewArr() };
+newBtn.onclick = function () { startNewArr() }; //set button to reset array
