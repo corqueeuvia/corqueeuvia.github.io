d1032c42d5e7f85d631211fe1c444eba496a65d7
caique <caique.ccali@gmail.com>
2022-02-01 14:22:17 -0300

    implemented logic, multiplier is working

---
+++ closure/assets/script.js
@@ -0,0 +1,14 @@
+function multiplyBy(number) {
+    return function multiply(byNumber) {
+        return display.innerHTML = (number * byNumber);
+    }
+}
+
+//capture html elements to get and display info
+const input = document.getElementById('input');
+const display = document.getElementById('display');
+
+//set multiplier to attribute to button
+const multiply2By = multiplyBy(2);
+const multiply5By = multiplyBy(5);
+const multiply10By = multiplyBy(10);
