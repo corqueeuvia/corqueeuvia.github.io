2a99b3320db41e33046c4b294626634606ae9b4c
caique <caique.ccali@gmail.com>
2022-02-01 18:03:38 -0300

    script done, all working + new style implemented

--- historinha/assets/script.js
+++ historinha/assets/script.js
@@ -1,7 +1,17 @@
-function increaseFontSize() {
-
+function newFontSize(size) {
+    return function resizeElement(element) {
+        for (let i = 0; i < element.length; i++) {
+            element[i].style.fontSize = `${size}pt`;
+        }
+        return true;
+    }
 }
 
-function decreaseFontSize() {
-    
-}
+const paragraphs = document.getElementsByTagName('p');
+
+const font12 = newFontSize(12);
+const font14 = newFontSize(14);
+const font18 = newFontSize(18);
+const font20 = newFontSize(20);
+const font22 = newFontSize(22);
+const font24 = newFontSize(24);
