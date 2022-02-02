eb9a8d20961a179368152d8be9511e75cd53d0ba
caique <caique.ccali@gmail.com>
2022-02-01 23:38:31 -0300

    mocking data to make testing easier

--- joanas-blacklist/assets/script.js
+++ joanas-blacklist/assets/script.js
@@ -3,9 +3,46 @@ const nameInput = document.getElementById('name-input');
 const dateInput = document.getElementById('date-input');
 const debtInput = document.getElementById('debt-input');
 const table = document.getElementById('display-table');
-const debtList = []; //array of objects to store data
-const clientNameList = [];
-const clientDateList = [];
+const debtList = [
+    { name: 'abgail', date: '2021-08-09', debt: '120' },
+    { name: 'christian', date: '2021-11-09', debt: '120' },
+    { name: 'beto', date: '2021-12-09', debt: '120' },
+    { name: 'diego', date: '2022-01-26', debt: '350' },
+    { name: 'edgar', date: '2022-02-21', debt: '780' },
+    { name: 'felix', date: '2022-02-03', debt: '800' },
+    { name: 'gertrudes', date: '2022-03-31', debt: '275' },
+    { name: 'hamilton', date: '2022-02-07', debt: '178' },
+    { name: 'irene', date: '2022-02-22', debt: '222' },
+    { name: 'joelma', date: '2021-10-05', debt: '189' },
+    { name: 'kevin', date: '2022-02-15', debt: '156' },
+    { name: 'leonas', date: '2022-02-21', debt: '570' },
+    { name: 'minerva', date: '2021-10-11', debt: '320' },
+    { name: 'neide', date: '2021-07-13', debt: '430' },
+    { name: 'onofre', date: '2021-05-11', debt: '570' },
+    { name: 'plínio', date: '2021-11-15', debt: '420' },
+    { name: 'quésia', date: '2021-04-20', debt: '660' },
+    { name: 'rihana', date: '2022-01-24', debt: '735' },
+    { name: 'samir', date: '2021-12-01', debt: '1000' },
+    { name: 'talia', date: '2021-12-14', debt: '1500' },
+    { name: 'ulisses', date: '2021-09-21', debt: '460' },
+    { name: 'valmir', date: '2021-08-16', debt: '230' },
+    { name: 'xuxu', date: '2021-11-18', debt: '200' },
+    { name: 'yara', date: '2022-01-31', debt: '330' },
+    { name: 'wilson', date: '2021-12-20', debt: '300' },
+    { name: 'zendaya', date: '2022-02-14', debt: '2560' },
+    { name: 'abgail', date: '2021-12-20', debt: '200' },
+    { name: 'christian', date: '2022-01-10', debt: '300' },
+    { name: 'beto', date: '2021-11-18', debt: '500' },
+    { name: 'abgail', date: '2021-11-08', debt: '100' },
+    { name: 'zendaya', date: '2021-11-08', debt: '150' },
+    { name: 'leonas', date: '2021-11-22', debt: '330' },
+    { name: 'onofre', date: '2021-07-12', debt: '600' },
+    { name: 'gertrudes', date: '2021-05-13', debt: '700' },
+]; //array of objects to store data
+const clientNameList = ['abgail', 'christian', 'beto', 'diego', 'edgar', 'felix', 'gertrudes', 'hamilton', 'irene', 'joelma', 'kevin', 'leonas', 'minerva', 'neide', 'onofre', 'plínio', 'quésia', 'rihana', 'samir', 'talia', 'ulisses', 'valmir', 'xuxu', 'yara', 'wilson', 'zendaya'];
+const clientDateList = ['2021-08-09', '2021-11-09', '2021-12-09', '2022-01-26', '2022-02-21', '2022-02-03', '2022-03-31', '2022-02-07', '2022-02-22', '2021-10-05', '2022-02-15', '2021-10-11', '2021-07-13', '2021-05-11', '2021-11-15', '2021-04-20', '2022-01-24', '2021-12-01', '2021-12-14', '2021-09-21', '2021-08-16', '2021-11-18', '2022-01-31', '2021-12-20', '2022-02-14', '2022-01-10', '2021-11-08', '2021-11-22', '2021-07-12', '2021-05-13'];
+
+addInfoToTable();
 
 function getNewInfo() { //gets info from inputs and stores in debtList array
     const newInfo = {};
